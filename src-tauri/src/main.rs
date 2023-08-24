e4c05ad674bd70f6130356885c0609315e395f1e
taiyou <hiromacha1116@icloud.com>
2023-08-24 14:49:28 +0900

    make 4 state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,12 +3,32 @@
     windows_subsystem = "windows"
 )]
 
-/**指定されたパスのファイル読み込み */
+// use dotenvy::dotenv;
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::{fs, io::Write};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Ticket {
+    key: String,
+    groupName: String,
+    contents: String,
+    itemType: String,
+}
+
+/**① 指定されたパスのファイル読み込み */
 fn read_file(path: String) -> String {
     std::fs::read_to_string(path).expect("could not read file")
 }
 
-/**2. パスを受け取り、読み込んだファイルを改行で区切りVecにして返す。 */
+fn read_file_path() -> String {
+    let tickets_file_path_key = "TICKETS_FILE_PATH";
+    // dotenv().ok();
+    // Ok(env);
+    env::var(tickets_file_path_key).unwrap()
+}
+
+/**② tauriのコマンド。ファイルの中身を改行で区切りVecにして返す。 */
 #[tauri::command]
 fn create_ticket(path: String) -> Vec<String> {
     println!("create ticket");
@@ -16,9 +36,33 @@ fn create_ticket(path: String) -> Vec<String> {
     tickets.lines().map(|s| s.to_string()).collect()
 }
 
+/**tauriのコマンド。渡されたチケットの配列をJSONで保存する。 */
+#[tauri::command]
+fn save_ticket(tickets: Vec<Ticket>) {
+    println!("save ticket");
+    let serialized: String = serde_json::to_string(&tickets).unwrap();
+    println!("{}", serialized);
+    let mut file = fs::File::create(read_file_path()).unwrap();
+    file.write_all(serialized.as_bytes()).unwrap();
+}
+
+/**tauriのコマンド。JSONファイルのチケット一覧を返却する。 */
+#[tauri::command]
+fn read_ticket() -> Vec<Ticket> {
+    println!("read ticket");
+    let ticket_json = fs::read_to_string(read_file_path()).unwrap();
+    println!("{}", ticket_json);
+    let ticket: Vec<Ticket> = serde_json::from_str(&ticket_json).unwrap();
+    return ticket;
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![create_ticket])
+        .invoke_handler(tauri::generate_handler![
+            create_ticket,
+            save_ticket,
+            read_ticket
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-import { open } from "@tauri-apps/api/dialog";
-import "./App.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { Draggable } from "./components/Draggable";
+import { Column } from "./components/Column";
+import { useTaskGroups } from "./hooks/useTaskGroup";
+import AppBar from "./components/AppBar";
+import { v4 as uuidv4 } from "uuid";
+import { ItemTypes } from "./types/itemTypes";
 
-function App() {
-  const [tickets, setTickets] = useState([""]);
+const GroupNameList = ["Backlog", "ToDo", "Doing", "Done"];
 
-  async function openDialog() {
-    /**1. @tauri-apps/api/dialog#openで選択したファイルのパスを取得。 */
-    const path = await open({ multiple: false });
-    if (typeof path === "string") {
-      /**2. create_ticketコマンドを実行。String[]で受け取ったチケット一覧をセット。 */
-      setTickets(await invoke<string[]>("create_ticket", { path: path })); 
-    }
-  }
+const newTaskGroup = GroupNameList.map((groupName) => ({
+  key: uuidv4(),
+  groupName: groupName,
+  contents: "",
+  itemType: ItemTypes.column,
+}));
 
-  const ticketsEl = tickets.map((ticket) => <p>{ticket}</p>);
+const App = () => {
+  const [swapTaskGroups, tasks, updateTasks, swapTasks, deleteTasks] =
+    useTaskGroups();
+
+  let index = 0;
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <button onClick={openDialog}>add tickets from file</button>
-        <button onClick={() => setTickets([])}>clear tickets</button>
+    <DndProvider backend={HTML5Backend}>
+      <div className="h-screen dark:bg-gray-800 dark:text-gray-50">
+        <AppBar tasks={tasks} />
+        <div className="h-full m-6 grid grid-cols-4 gap-4 place-items-stretch">
+          {newTaskGroup.map((taskGroup, columnIndex) => {
+            const groupedTasks = tasks.filter((task) => {
+              return task.groupName === taskGroup.groupName;
+            });
+            const firstIndex = index;
+            index = index + groupedTasks.length;
+            return (
+              <li key={taskGroup.key} className="list-none">
+                <Draggable
+                  item={taskGroup}
+                  index={columnIndex}
+                  swapItems={swapTaskGroups}
+                >
+                  <Column
+                    columnName={taskGroup.groupName}
+                    firstIndex={firstIndex}
+                    tasks={groupedTasks}
+                    updateTasks={updateTasks}
+                    deleteTasks={deleteTasks}
+                    swapTasks={swapTasks}
+                  ></Column>
+                </Draggable>
+              </li>
+            );
+          })}
+        </div>
       </div>
-
-      {ticketsEl}
-    </div>
+    </DndProvider>
   );
-}
+};
 
 export default App;
